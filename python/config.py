ce3a2cac6488457ef0c64a2f99ad22cce6b48753
Eric Wieser <wieser.eric@gmail.com>
2016-11-01 01:52:24 +0000

    Add some configuration data about the servos

---
+++ python/config.py
@@ -0,0 +1,12 @@
+N = 3
+
+# the pulses corresponding to 0 and 90 degrees
+servo_0_90 = [
+	(1205, 2031),
+	(1396, 2274),
+	(1157, 2022)
+]
+
+# the limits beyond which the servo can't tell the difference, and might cause
+# damage
+servo_limits = (550, 2300)
