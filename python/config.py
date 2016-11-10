20c49910e8e90abf5b859c6de0893e011982188b
Eric Wieser <wieser.eric@gmail.com>
2016-11-10 14:30:31 +0000

    Tweak servo angle config

--- python/config.py
+++ python/config.py
@@ -4,9 +4,9 @@ N = 3
 
 # the pulses corresponding to 0 and 90 degrees
 servo_0_90 = np.array([
-	[1205, 2031],
-	[1396, 2274],
-	[1157, 2022]
+	[1170, 2040],
+	[1410, 2274],
+	[1217, 2055]
 ])
 servo_per_radian = (servo_0_90[:,1] - servo_0_90[:,0]) / np.radians(90)
 servo_0 = servo_0_90[:,0]
