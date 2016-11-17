b9ac65cf9805b897b0ac300bc1c51677a7a3f1f5
Angus <ab2164@cam.ac.uk>
2016-11-17 23:20:56 +0000

    Changed measure_parameters to test joint angles 2 and 3 under gravity

--- python/measure_parameters.py
+++ python/measure_parameters.py
@@ -11,19 +11,59 @@ from robot import Robot
 #from robot import SimulatedRobot as Robot
 
 with Robot.connect() as r, ui.basic(r) as gui:
-    for i in range(3):
-    	angles = np.linspace(-90,90,500)
 
-    	for a in angles:
-    		r.servo_angle = np.radians([0,0,a])
-    		time.sleep(0.02)
 
-    	angles = angles[::-1]
+    # angles = np.linspace(-90,90,11)
+    # angles = np.concatenate((angles, angles[::-1]))
+    # angles = np.tile(angles,3)
 
-    	for a in angles:
-    		r.servo_angle = np.radians([0,0,a])
-    		time.sleep(0.02)
+    # with open('sausages.tsv','w') as f:
+    #     for a in angles:
+    #         r.servo_angle = np.radians([-45,-45,a])
+    #         time.sleep(0.5)
+    #         print(r.joint_angles[2], r.angle_error[2], sep='\t', file=f)
 
+
+    angles_pos = np.stack((
+        np.linspace(-90,0,51),
+        np.linspace(0,-90,51)
+    ), axis=-1)
+   
+    angles_0 = np.stack((
+        np.linspace(0,0,91),
+        np.linspace(-90,90,91)
+    ), axis=-1)
+
+    angles_neg = np.stack((
+        np.linspace(0,90,51),
+        np.linspace(90,0,51)
+    ), axis=-1)
+
+    angles = np.concatenate((angles_pos, angles_0, angles_neg))
+    angles = np.concatenate((angles, angles[::-1]))
+    angles = np.tile(angles,(3,1))
+    print(angles)
+
+    with open('moresausages.tsv','w') as f:
+        for i,(a,b) in enumerate(angles):
+            r.servo_angle = np.radians([45,a,b])
+            time.sleep(0.05)
+            if i% 10 == 0:
+                time.sleep(0.25)
+                print(r.joint_angles[1], r.angle_error[1], r.joint_angles[2], r.angle_error[2], sep='\t', file=f)
+
+    # turned upside down (relative to first test) to avoid wire interference
+
+
+    # angles = np.linspace(-90,90,11)
+    # angles = np.concatenate((angles, angles[::-1]))
+    # angles = np.tile(angles,3)
+
+    # with open('sausages.tsv','w') as f:
+    #     for a in angles:
+    #         r.servo_angle = np.radians([-45,-45,a])
+    #         time.sleep(0.5)
+    #         print(r.servo_angle[2], r.angle_error[2], sep='\t', file=f)
 
         # t = time.time()
         # f = 0.5  # Hz
