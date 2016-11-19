34eb0212091d15e3191bc23320479eaf21e14bda
sineadey <sineaad@gmail.com>
2016-11-19 19:18:32 +0000

    fixes q1 at 0

--- python/invkin.py
+++ python/invkin.py
@@ -18,31 +18,31 @@ def get_sympy_jacobian():
 	x = -(lengths[0]*sin(qq1) + lengths[1]*sin(qq1+qq2) + lengths[2]*sin(qq1+qq2+qq3)+ lengths[3]*sin(qq1+qq2+qq3+qq4))
 	y = lengths[0]*cos(qq1) + lengths[1]*cos(qq1+qq2) + lengths[2]*cos(qq1+qq2+qq3) + lengths[3]*cos(qq1+qq2+qq3+qq4)
 
-	J1 =[diff(x,qq1), diff(x,qq2), diff(x,qq3), diff(x,qq4)]
-	J2 =[diff(y,qq1), diff(y,qq2), diff(y,qq3), diff(y,qq4)]
+	J1 =[diff(x,qq2), diff(x,qq3), diff(x,qq4)]
+	J2 =[diff(y,qq2), diff(y,qq3), diff(y,qq4)]
 
 	return np.array([J1, J2])
 
 def pJ(qq):
 	from numpy import sin, cos
-	qq1 = qq[0]
-	qq2 = qq[1]
-	qq3 = qq[2]
-	qq4 = qq[3]
+	qq1 = 0
+	qq2 = qq[0]
+	qq3 = qq[1]
+	qq4 = qq[2]
 	#J1 = [-lengths[0]*cos(qq1) - lengths[1]*cos(qq1 + qq2) - lengths[2]*cos(qq1 + qq2 + qq3) - lengths[3]*cos(qq1 + qq2 + qq3 + qq4), -lengths[1]*cos(qq1 + qq2) - lengths[2]*cos(qq1 + qq2 + qq3) - lengths[3]*cos(qq1 + qq2 + qq3 + qq4), -lengths[2]*cos(qq1 + qq2 + qq3) - lengths[3]*cos(qq1 + qq2 + qq3 + qq4), -lengths[3]*cos(qq1 + qq2 + qq3 + qq4)]
 	#J2 = [-lengths[0]*sin(qq1) - lengths[1]*sin(qq1 + qq2) - lengths[2]*sin(qq1 + qq2 + qq3) - lengths[3]*sin(qq1 + qq2 + qq3 + qq4), -lengths[1]*sin(qq1 + qq2) - lengths[2]*sin(qq1 + qq2 + qq3) - lengths[3]*sin(qq1 + qq2 + qq3 + qq4), -lengths[2]*sin(qq1 + qq2 + qq3) - lengths[3]*sin(qq1 + qq2 + qq3 + qq4), -lengths[3]*sin(qq1 + qq2 + qq3 + qq4)]
 	J = np.array([
-		[-lengths[0]*cos(qq1) - lengths[1]*cos(qq1 + qq2) - lengths[2]*cos(qq1 + qq2 + qq3) - lengths[3]*cos(qq1 + qq2 + qq3 + qq4), -lengths[1]*cos(qq1 + qq2) - lengths[2]*cos(qq1 + qq2 + qq3) - lengths[3]*cos(qq1 + qq2 + qq3 + qq4), -lengths[2]*cos(qq1 + qq2 + qq3) - lengths[3]*cos(qq1 + qq2 + qq3 + qq4), -lengths[3]*cos(qq1 + qq2 + qq3 + qq4)],
-		[-lengths[0]*sin(qq1) - lengths[1]*sin(qq1 + qq2) - lengths[2]*sin(qq1 + qq2 + qq3) - lengths[3]*sin(qq1 + qq2 + qq3 + qq4), -lengths[1]*sin(qq1 + qq2) - lengths[2]*sin(qq1 + qq2 + qq3) - lengths[3]*sin(qq1 + qq2 + qq3 + qq4), -lengths[2]*sin(qq1 + qq2 + qq3) - lengths[3]*sin(qq1 + qq2 + qq3 + qq4), -lengths[3]*sin(qq1 + qq2 + qq3 + qq4)]
+		[ -0.148*cos(qq1 + qq2) - 0.149*cos(qq1 + qq2 + qq3) - 0.139*cos(qq1 + qq2 + qq3 + qq4), -0.149*cos(qq1 + qq2 + qq3) - 0.139*cos(qq1 + qq2 + qq3 + qq4), -0.139*cos(qq1 + qq2 + qq3 + qq4)],
+		[ -0.148*sin(qq1 + qq2) - 0.149*sin(qq1 + qq2 + qq3) - 0.139*sin(qq1 + qq2 + qq3 + qq4), -0.149*sin(qq1 + qq2 + qq3) - 0.139*sin(qq1 + qq2 + qq3 + qq4), -0.139*sin(qq1 + qq2 + qq3 + qq4)]
 	])
 	return np.linalg.pinv(J)
 
 def f(qq):
 	from numpy import sin, cos
-	qq1 = qq[0]
-	qq2 = qq[1]
-	qq3 = qq[2]
-	qq4 = qq[3]
+	qq1 = 0
+	qq2 = qq[0]
+	qq3 = qq[1]
+	qq4 = qq[2]
 	#x = -(lengths[0]*sin(qq1) + lengths[1]*sin(qq1+qq2) + lengths[2]*sin(qq1+qq2+qq3)+ lengths[3]*sin(qq1+qq2+qq3+qq4))
 	#y = lengths[0]*cos(qq1) + lengths[1]*cos(qq1+qq2) + lengths[2]*cos(qq1+qq2+qq3) + lengths[3]*cos(qq1+qq2+qq3+qq4)
 	return np.array([-(lengths[0]*sin(qq1) + lengths[1]*sin(qq1+qq2) + lengths[2]*sin(qq1+qq2+qq3)+ lengths[3]*sin(qq1+qq2+qq3+qq4)), lengths[0]*cos(qq1) + lengths[1]*cos(qq1+qq2) + lengths[2]*cos(qq1+qq2+qq3) + lengths[3]*cos(qq1+qq2+qq3+qq4)], dtype=np.float64)
@@ -54,23 +54,23 @@ def get_servo_angles(r):
 	# r is the desired coordinate of the foot that is not the base foot (the end effector)
 	r = np.asarray(r)
 	# change starting angles
-	q1 = 2.0
+	q1 = 0
 	q2 = 2.0
 	q3 = 2.0
 	q4 = 2.0
-	q = np.array([q1, q2, q3, q4], dtype=np.float64)
+	q = np.array([q2, q3, q4], dtype=np.float64)
 
 	# find set of angles (q) to get to r
 	for i in range(1,200):
 		q = q + np.dot(pJ(q),(r-f(q)))
 
 	# change q so that it is between 0 and 2pi
-	q = q % 2*np.pi
+	q = q % (2*np.pi)
 
-	# print(f(q)) # checking that end effector is in the right location for the new q
+	#print(f(q)) # checking that end effector is in the right location for the new q
 
 	# remember q[0][0] is phi1, q[1][0] is phi2, q[2][0] is phi3, q[3][0] is phi4
-	return q[1:]
+	return q[0:]
 
 if __name__ == '__main__':
 	print(get_servo_angles([0.3,0.3]))
