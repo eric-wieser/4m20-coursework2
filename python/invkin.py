3b2f8671fd69b2b6af974762f1a935ff1603603f
Eric Wieser <wieser.eric@gmail.com>
2016-11-19 18:12:59 +0000

    Make the sympy stuff optional, so that people without this can run it

--- python/invkin.py
+++ python/invkin.py
@@ -1,32 +1,32 @@
-# angles defined in the same way as in the coursework, 
+# angles defined in the same way as in the coursework,
 # but all angles are clockwise in the positive direction
 
 # phi1, phi2, phi3 are q1, q2 and q3
 
 import numpy as np
 from numpy import sin, cos
-from sympy import *
 from functions import pJ, f
 from math import pi
-
-# Calculating J and pinvJ using symbolic algebra - only have to do this if J & pinvJ need to be recalculated
-qq1 = Symbol('qq1')
-qq2 = Symbol('qq2')
-qq3 = Symbol('qq3')
-qq4 = Symbol('qq4')
 
 # the length of the links, in meters
 from config import lengths
 
-x = -(lengths[0]*sin(qq1) + lengths[1]*sin(qq1+qq2) + lengths[2]*sin(qq1+qq2+qq3)+ lengths[3]*sin(qq1+qq2+qq3+qq4))
-y = lengths[0]*cos(qq1) + lengths[1]*cos(qq1+qq2) + lengths[2]*cos(qq1+qq2+qq3) + lengths[3]*cos(qq1+qq2+qq3+qq4)
+def get_sympy_jacobian():
+	from sympy import Symbol, diff, sin, cos
+	# Calculating J and pinvJ using symbolic algebra - only have to do this if J & pinvJ need to be recalculated
+	qq1 = Symbol('qq1')
+	qq2 = Symbol('qq2')
+	qq3 = Symbol('qq3')
+	qq4 = Symbol('qq4')
 
-J1 =[diff(x,qq1), diff(x,qq2), diff(x,qq3), diff(x,qq4)]
-J2 =[diff(y,qq1), diff(y,qq2), diff(y,qq3), diff(y,qq4)]
 
-#print(["J1 =",J1],["J2=",J2]) # uncomment to print J out
+	x = -(lengths[0]*sin(qq1) + lengths[1]*sin(qq1+qq2) + lengths[2]*sin(qq1+qq2+qq3)+ lengths[3]*sin(qq1+qq2+qq3+qq4))
+	y = lengths[0]*cos(qq1) + lengths[1]*cos(qq1+qq2) + lengths[2]*cos(qq1+qq2+qq3) + lengths[3]*cos(qq1+qq2+qq3+qq4)
 
-J = np.array([J1, J2]) 
+	J1 =[diff(x,qq1), diff(x,qq2), diff(x,qq3), diff(x,qq4)]
+	J2 =[diff(y,qq1), diff(y,qq2), diff(y,qq3), diff(y,qq4)]
+
+	return np.array([J1, J2])
 
 def getservos(xcoord,ycoord):
 	# returns a set of servo values to send to the robot
@@ -35,7 +35,7 @@ def getservos(xcoord,ycoord):
 
 	# r is the desired coordinate of the foot that is not the base foot (the end effector)
 	r = np.array([[xcoord], [ycoord]], dtype=np.float64)
-	# change starting angles 
+	# change starting angles
 	q1 = 2.0
 	q2 = 2.0
 	q3 = 2.0
@@ -61,6 +61,6 @@ def getservos(xcoord,ycoord):
 	servos = np.array([q[1][0]*sv[0], q[2][0]*sv[1], q[3][0]*sv[2]])
 	return(servos)
 
-# print(getservos(0.3,0.3)) 
-
-
+if __name__ == '__main__':
+	print(get_servo_angles(0.3,0.3))
+	print("Jacobian is", get_sympy_jacobian())
