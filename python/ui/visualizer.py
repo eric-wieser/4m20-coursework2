daf25f77f9d532364eab4cc5784eb9d614438870
Eric Wieser <wieser.eric@gmail.com>
2016-11-20 17:39:06 +0000

    Draw joint forces

--- python/ui/visualizer.py
+++ python/ui/visualizer.py
@@ -4,6 +4,7 @@ import numpy as np
 
 class GeometryVisualizer(tk.Canvas):
     SCALE = 400 # pixels per meter
+    FORCE_SIZE = 0.05
     def __init__(self, master, *, robot, **kwargs):
         super().__init__(master, **kwargs)
         self.robot = robot
@@ -20,6 +21,21 @@ class GeometryVisualizer(tk.Canvas):
             self.create_line(0, 0, 200, 0, **props),
         ]
 
+        self.joints = [
+            self.create_arc(-
+                -self.FORCE_SIZE,
+                -self.FORCE_SIZE,
+                self.FORCE_SIZE,
+                self.FORCE_SIZE,
+                start=0,
+                extent=0,
+                outline='green',
+                width=self.SCALE * 0.0125,
+                style='arc'
+            )
+            for i in range(3)
+        ]
+
         self.__update()
 
     def on_resize(self, event):
@@ -33,13 +49,43 @@ class GeometryVisualizer(tk.Canvas):
 
     def __update_once(self):
         last = np.zeros(2)
-        for link, pos in zip(self.links, self.robot.joint_positions):
+        poss = self.robot.joint_positions
+        angles = self.robot.link_angles
+        forces = self.robot.angle_error
+        stalled = self.robot.joints_stalled
+
+        for link, pos in zip(self.links, poss):
             coords = np.array([last, pos])
             coords = self._to_screen_coords(coords)
             if np.isnan(coords).any():
                 return
             self.coords(link, *coords.ravel())
             last = pos
+
+        for i, joint in enumerate(self.joints):
+            posi = poss[i] + self.FORCE_SIZE*np.array([
+                [-1, -1],
+                [1, 1]
+            ])
+            coords = self._to_screen_coords(posi)
+            self.coords(joint, *coords.ravel())
+            extent = np.degrees(forces[i])
+            start = -np.degrees(angles[i])
+
+            if extent < 0:
+                start += 5
+                extent -= 10
+            else:
+                start -= 5
+                extent += 10
+            self.itemconfig(joint,
+                start=start,
+                extent=extent
+            )
+            if stalled[i]:
+                self.itemconfig(joint, outline='red')
+            else:
+                self.itemconfig(joint, outline='green')
 
     def __update(self):
         self.__update_once()
