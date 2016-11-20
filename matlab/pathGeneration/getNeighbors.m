54b118bcea84c4eea033e5132105e6f3a2ee7454
Hugo Hadfield <hadfield.hugo@gmail.com>
2016-11-20 11:54:42 +0000

    Added multiple joints moving at once

--- matlab/pathGeneration/getNeighbors.m
+++ matlab/pathGeneration/getNeighbors.m
@@ -1,16 +1,17 @@
 function [ cellout ] = getNeighbors( currentPoint, stepsize )
 %GETNEIGHBORS Summary of this function goes here
 %   Detailed explanation goes here
-
 numCurr = inverseHashFunction( currentPoint );
-cellout{1} = hashFunction( [ numCurr(1) + stepsize; numCurr(2); numCurr(3) ] );
-cellout{2} = hashFunction( [ numCurr(1) - stepsize; numCurr(2); numCurr(3) ]) ;
-
-cellout{3} = hashFunction( [ numCurr(1) ; numCurr(2)+ stepsize; numCurr(3) ]) ;
-cellout{4} = hashFunction( [ numCurr(1) ; numCurr(2)- stepsize; numCurr(3) ]) ;
-
-cellout{5} = hashFunction( [ numCurr(1); numCurr(2); numCurr(3) + stepsize ]) ;
-cellout{6} = hashFunction( [ numCurr(1); numCurr(2); numCurr(3) - stepsize ]) ;
+cellout = cell(3*3*3,1);
+n = 1;
+for i = [stepsize,0,-stepsize]
+    for j = [stepsize,0,-stepsize]
+        for k = [stepsize,0,-stepsize]
+            cellout{n} = hashFunction( [ numCurr(1) + i; numCurr(2) + j; numCurr(3) + k ] );
+            n = n+1;
+        end
+    end
+end
 
 end
 
