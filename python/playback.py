2d3746192ca274c113a4eb1c3e8a8ac9dcfeb0eb
Eric Wieser <wieser.eric@gmail.com>
2016-11-24 13:46:28 +0000

    Add step timings

--- python/playback.py
+++ python/playback.py
@@ -8,26 +8,30 @@ import ui
 from scipy.interpolate import interp1d
 #from robot import SimulatedRobot as Robot
 
-gaitsequence=np.loadtxt('onestep.csv',delimiter=',')
+gaitsequence = np.loadtxt('onestep.csv',delimiter=',')
+gaitsequence = np.concatenate((gaitsequence, gaitsequence[0:1]))
+dts = gaitsequence[:,0]
+angles = gaitsequence[:,1:]
+period = np.sum(dts[:-1])
+times = np.zeros(len(angles))
+times[1:] = np.cumsum(dts[:-1])
+
 is_first = True
 
-gaitsequence = np.concatenate((gaitsequence, gaitsequence[0:1]))
+# append the last one to the start
 N = len(gaitsequence)
 t=np.arange(N)
 
 tinterp = np.arange((N-1) * 100) / 100
 
-gaitsequence = interp1d(t, gaitsequence.T)(tinterp).T
+angle_for = interp1d(times, angles.T)
+
 with Robot.connect() as r, ui.basic(r) as gui:
+    r.target_joint_angle = angles[0]
+    time.sleep(2)
+    start_time = time.time()
     while True:
-        for i,row in enumerate(gaitsequence):
-            if np.isnan(row).any():
-                time.sleep(2)
-                continue
-            r.target_joint_angle = np.radians(row)
-            print(row)
-            print(i)
-            time.sleep(5 if is_first else 0.5 / 100)
-            is_first = False
-            if not gui.open:
-                raise SystemExit
+        t = (time.time() - start_time) % period
+        r.target_joint_angle = np.radians(angle_for(t))
+        if not gui.open:
+            raise SystemExit
